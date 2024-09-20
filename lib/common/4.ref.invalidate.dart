4a9626bb043ba74023d05249297838f8aa6ec139
chandara-dgc <L.Chandara@dgc.gov.kh>
2024-09-20 13:28:10 +0700

    build: declarative

--- lib/common/4.ref.invalidate.dart
+++ lib/common/4.ref.invalidate.dart
@@ -1,1 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+void main() {
+  runApp(ProviderScope(
+      child: MaterialApp(
+    home: Scaffold(body: Center(child: ReadExample())),
+  )));
+}
+
+/// `ref.invalidate`
+/// ref.invalidate is used to invalidate a provider, which means its cached value is discarded and it will recompute its state the next time it's accessed.
+/// Unlike ref.refresh, it doesn’t force an immediate recomputation but waits until the next access.
+
+final counterProvider = StateProvider<int>((ref) => 0);
+
+class ReadExample extends ConsumerWidget {
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final counter = ref.watch(counterProvider);
+
+    return Scaffold(
+      appBar: AppBar(title: Text('ref.read Example')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Counter: $counter'),
+            ElevatedButton(
+              onPressed: () {
+                ref.invalidate(counterProvider);
+              },
+              child: Text('invaldate'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
